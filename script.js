203a924db640c2ca633294153d716a16a329b05f
ussi-alok <81419246+ussi-alok@users.noreply.github.com>
2023-08-14 17:07:14 +0530

    Added background color to main container: for better visibility

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const body = document.querySelector('body');
 
 function createMainContainer() {
+    const containerDimension = 550;
     const mainContainer = document.createElement('div');
     mainContainer.setAttribute('class','mainContainer');
+    mainContainer.style.cssText=`height:${containerDimension}px;
+                                width:${containerDimension}px;
+                                background-color:red`;
     return mainContainer;
 };
 
