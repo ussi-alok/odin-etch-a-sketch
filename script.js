d65f773fa26d7fa75eb628752a4a00b1e511f702
ussi-alok <81419246+ussi-alok@users.noreply.github.com>
2023-08-14 23:34:27 +0530

    Created subContainer within loop

--- script.js
+++ script.js
@@ -2,27 +2,38 @@ const body = document.querySelector('body');
 
 
 const containerDimension = 550;
+const noOfBox = 8;
+const smallContainerDimension = containerDimension/noOfBox;
 
 function createMainContainer() {
     const mainContainer = document.createElement('div');
     mainContainer.setAttribute('class','mainContainer');
     mainContainer.style.cssText=`height:${containerDimension}px;
                                 width:${containerDimension}px;
-                                background-color:red`;
+                                background-color:red;
+                                display:flex;
+                                flex-wrap:wrap`;
     return mainContainer;
 };
 
 function createSmallContainer() {
     const smallContainer = document.createElement('div');
     smallContainer.setAttribute('class','smallContainer');
-    smallContainer.style.cssText=`height:${containerDimension/2}px;
-                                width:${containerDimension/2}px;
-                                background-color:black`;
+    smallContainer.style.cssText=`height:${smallContainerDimension -1}px;
+                                width:${smallContainerDimension -1.3}px;
+                                background-color:black;
+                                margin:0.5px`;
     return smallContainer;
 };
 
 const mainContainer = createMainContainer();
-const smallContainer = createSmallContainer();
 
-mainContainer.appendChild(smallContainer);
+
+for (let index = 0; index < noOfBox**2; index++) {
+    const smallContainer = createSmallContainer();
+    mainContainer.appendChild(smallContainer);
+    
+}
+
+
 body.appendChild(mainContainer);
